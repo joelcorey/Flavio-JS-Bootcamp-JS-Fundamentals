93c13b7e5acf1ffa0d9682a771f9a3dc9a6411ed
Joel Corey <joelcorey@fastmail.com>
2020-08-03 13:40:48 -0700

    move withRooms to its own file

--- app.js
+++ app.js
@@ -5,5 +5,12 @@ This keeps everything nice and tidy
 import House from './house';
 import withRooms from './rooms';
 
+/**
+Unlike functions classes are not hoisted. They
+must be declared before use. Functions are read in to
+memory first(hoisting) so out of order calling of functions
+is okay in JavaScript. Classes are not read in to memoery in
+this fashion.
+*/
 let myHouse = new House(withRooms);
 
